5e5e0bfeda54208f1022278b8d3ec94f25bc5913
Jenneeee <jenne@cattoor.net>
2022-04-27 21:43:45 +0200

    Started on showing clubs

--- src/components/Club.js
+++ src/components/Club.js
@@ -1,20 +1,14 @@
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Image from '../static/images/dumbell.jpg';
 
-function Club() {
+function Club({ club }) {
+    console.log(club)
     return (
-        <Card sx={{ maxWidth: 180, boxShadow: 0, margin: '0 1rem' }}>
+        <Card sx={{ boxShadow: 0, margin: '0 1rem' }}>
             <CardContent sx={{ padding: 0 }}>
-                <CardMedia
-                    component="img"
-                    alt="Image of Dumbells"
-                    height="90"
-                    image={Image}
-                />
-                <Typography variant="h3">Basic-Fit Knokke-Heist Duinenwater</Typography>
+                <Typography variant="h3" component="h3" sx={{ marginTop: '.5rem' }}>{club.attributes.name}<br></br>{club.attributes.address}</Typography>
+                <Typography variant="h4"></Typography>
             </CardContent>
         </Card>
     );
--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -1,15 +1,31 @@
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Typography, Box, Alert, CircularProgress } from '@mui/material';
 import Club from '../components/Club';
+import useFetch from '../hooks/useFetch';
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 function Clubs() {
+
+    const { data: clubs, isLoading, error } = useFetch(`${backendUrl}/api/clubs?populate=*`);
+
+    if (isLoading) {
+        return <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh">
+            <CircularProgress />
+        </Box>
+    }
     return (
         <>
             <Typography variant="title" component="h1">Clubs</Typography>
             <Box sx={{ color: '#fff', background: 'linear-gradient(0.25turn, #FF9D26, #FE7000);', paddingBottom: '1.5rem' }}>
                 <Typography variant="h2">My favourite clubs</Typography>
-                <Club />
             </Box>
+            {error && <Alert severity="error">Something went wrong with loading your profile</Alert>}
+            {clubs && clubs.data.map(club => <Club club={club} />)}
         </>
     );
 }
