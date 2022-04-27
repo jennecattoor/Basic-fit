d9aa2cf9289b252b024145e6559f5b8baec9871a
Jenneeee <jenne@cattoor.net>
2022-04-27 10:20:31 +0200

    Fixing bugs with adding and removing favourite workouts

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -1,56 +1,47 @@
-import { Typography, Card, CardContent, CardMedia, Box, Fab, CircularProgress, Alert } from '@mui/material/';
+import { Typography, Card, CardContent, CardMedia, Box, Fab } from '@mui/material/';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { Link } from 'react-router-dom'
-import useFetch from '../hooks/useFetch';
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const profileId = parseInt(localStorage.getItem('profileId'));
 
-function WorkoutCard({ workout, color, id }) {
+function WorkoutCard({ workout, color, id, favouriteWorkouts }) {
 
-    const { data: profiles, isLoading, error } = useFetch(`${backendUrl}/api/profiles?populate=*`);
-
-    const addFavourites = () => {
-        const profileId = parseInt(localStorage.getItem('profileId'));
-        const userProfile = profiles.data.find(user => user.id === profileId)
-        const favouriteWorkoutsList = userProfile.attributes.favouriteWorkouts.data.map(item => item.id).flat()
-
-        if (!favouriteWorkoutsList.find(item => item === id)) {
-            console.log('Item added')
-            favouriteWorkoutsList.push(id)
-            fetch(`${backendUrl}/api/profiles/${profileId}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ data: { favouriteWorkouts: favouriteWorkoutsList } }),
+    const putFavourites = (ids) => {
+        fetch(`${backendUrl}/api/profiles/${profileId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data: { favouriteWorkouts: ids } }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
             })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Success:', data);
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    const checkFavourites = () => {
+        if (!favouriteWorkouts.find(item => item === id)) {
+            console.log('Item added')
+            favouriteWorkouts.push(id)
+            putFavourites(favouriteWorkouts)
         }
         else {
-            console.log('item is already in favourites')
+            console.log('item removed')
+            const filteredFavourites = favouriteWorkouts.filter(item => item !== id)
+            putFavourites(filteredFavourites)
         }
     }
 
-    if (isLoading) {
-        return <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            minHeight="100vh">
-            <CircularProgress />
-        </Box>
-    }
+
     return (
         <Card sx={{ maxWidth: 225, boxShadow: 0, margin: '.5rem 1rem', background: "none" }}>
             <CardContent sx={{ padding: 0 }}>
-                <Box sx={{ position: "relative" }} onClick={() => addFavourites()}>
+                <Box sx={{ position: "relative" }} onClick={() => checkFavourites()}>
                     <Fab sx={{ position: 'absolute', top: 10, right: 10, boxShadow: 'none', zIndex: 0 }} size="small" aria-label="favourites" >
                         <FavoriteIcon />
                     </Fab>
@@ -61,7 +52,6 @@ function WorkoutCard({ workout, color, id }) {
                         alt={workout.image.data.attributes.alternativeText}
                         image={workout.image.data.attributes.formats.small.url}
                     />
-                    {error && <Alert severity="error">Something went wrong</Alert>}
                     <Typography variant="h2" sx={{ color: color, padding: '.5rem 0 0 0' }}>{workout.name}</Typography>
                     <Typography variant="body" sx={{ color: color, padding: '0' }}>{workout.level} · {workout.duration} min</Typography>
                 </Link >
--- src/pages/Workouts.js
+++ src/pages/Workouts.js
@@ -5,8 +5,17 @@ import useFetch from '../hooks/useFetch';
 function Workouts() {
     const backendUrl = process.env.REACT_APP_BACKEND_URL;
     const { data: workouts, isLoading, error } = useFetch(`${backendUrl}/api/workouts?populate=*`);
+    const { data: profiles, isLoading: profileLoading, error: profileError } = useFetch(`${backendUrl}/api/profiles?populate=*`);
 
-    if (isLoading) {
+    var favouriteWorkoutsList = [];
+    if (profiles) {
+        const profileId = parseInt(localStorage.getItem('profileId'));
+        const userProfile = profiles.data.find(user => user.id === profileId)
+        favouriteWorkoutsList = userProfile.attributes.favouriteWorkouts.data.map(item => item.id)
+    }
+
+
+    if (isLoading || profileLoading) {
         return <Box
             display="flex"
             flexDirection="column"
@@ -16,16 +25,18 @@ function Workouts() {
             <CircularProgress />
         </Box>
     }
+
     return (
         <>
             <Typography variant="title" component="h1">Workouts</Typography>
             <Box sx={{ color: '#fff', background: 'linear-gradient(0.25turn, #FF9D26, #FE7000);' }}>
                 <Typography variant="h2">New</Typography>
-                {error && <Alert severity="error">Something went wrong</Alert>}
-                <Grid container>{workouts && workouts.data.slice(workouts.data.length - 2, workouts.data.length).reverse().map(workout => <Grid item xs={6} key={"workout" + workout.id}><WorkoutCard workout={workout.attributes} id={workout.id} color="#fff" /></Grid>)}</Grid>
+                {error && <Alert severity="error">Something went wrong with loading the workouts</Alert>}
+                {profileError && <Alert severity="error">Something went wrong with loading your profile</Alert>}
+                <Grid container>{workouts && profiles && workouts.data.slice(workouts.data.length - 2, workouts.data.length).reverse().map(workout => <Grid item xs={6} key={"workout" + workout.id}><WorkoutCard workout={workout.attributes} id={workout.id} color="#fff" favouriteWorkouts={favouriteWorkoutsList} /></Grid>)}</Grid>
             </Box>
             <Typography variant="h2">All</Typography>
-            <Grid container>{workouts && workouts.data.slice(0, workouts.data.length - 2).map(workout => <Grid item xs={6} key={"workout" + workout.id}><WorkoutCard workout={workout.attributes} id={workout.id} color="#2d2d2d" /></Grid>)}</Grid>
+            <Grid container>{workouts && profiles && workouts.data.slice(0, workouts.data.length - 2).map(workout => <Grid item xs={6} key={"workout" + workout.id}><WorkoutCard workout={workout.attributes} id={workout.id} color="#2d2d2d" favouriteWorkouts={favouriteWorkoutsList} /></Grid>)}</Grid>
         </>
     );
 }
