a0fc3860ae857c7922fdf8ca342848c0b1a5bf82
Jenneeee <jenne@cattoor.net>
2022-04-29 15:29:42 +0200

    Using react query on Clubs page + making able to remove

--- src/components/Club.js
+++ src/components/Club.js
@@ -1,12 +1,61 @@
-import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import { Typography, Box, Fab, Card, CardContent, CardMedia, CircularProgress, Alert } from '@mui/material';
 import Image from '../static/images/dumbell.jpg';
+import CloseIcon from '@mui/icons-material/Close';
+import { useQueryClient, useQuery, useMutation } from "react-query";
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const profileId = parseInt(localStorage.getItem('profileId'));
 
 function Club({ club }) {
+
+    const { data: clubs, isLoading, error } = useQuery("clubs", async () => {
+        const data = await fetch(`${backendUrl}/api/clubs?populate=*`).then(r => r.json());
+        return data;
+    });
+
+    const queryClient = useQueryClient()
+    const putFavouriteClub = async (data) => {
+        const favourites = clubs.data.filter(club => club.attributes.favouriteProfiles.data.find(item => item.id === profileId)).map(item => item.id)
+        const newFavourites = favourites.filter(id => id !== data)
+        return await fetch(`${backendUrl}/api/profiles/${profileId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ data: { favouriteClubs: newFavourites } }),
+        }).then(r => r.json());
+    }
+
+    const mutation = useMutation(putFavouriteClub, {
+        onSuccess: () => {
+            console.log("success")
+            queryClient.invalidateQueries("clubs");
+        },
+    })
+
+    const removeClub = data => {
+        mutation.mutate(data)
+    }
+
+    if (isLoading) {
+        return <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh">
+            <CircularProgress />
+        </Box>
+    }
+
     return (
         <Card sx={{ maxWidth: 180, boxShadow: 0, margin: '.5rem  1rem' }}>
+            {error && <Alert severity="error">Something went wrong with loading the clubs</Alert>}
+            <Box sx={{ position: "relative" }} onClick={() => removeClub(club.id)}>
+                <Fab sx={{ position: 'absolute', top: 5, right: 5, boxShadow: 'none', zIndex: 0 }} size="small" aria-label="favourites">
+                    <CloseIcon />
+                </Fab>
+            </Box>
             <CardContent sx={{ padding: 0 }}>
                 <CardMedia
                     component="img"
--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -1,45 +1,53 @@
-import { Typography, Box, Alert, CircularProgress, Autocomplete, TextField, Button, Grid } from '@mui/material';
+import { Typography, Box, Alert, CircularProgress, Autocomplete, TextField, Button, Grid, Stack } from '@mui/material';
+import { QueryClient, QueryClientProvider, useQuery, useMutation } from "react-query";
+import { useForm } from "react-hook-form";
 import Club from '../components/Club';
-import useFetch from '../hooks/useFetch';
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 
+const queryClient = new QueryClient();
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const profileId = parseInt(localStorage.getItem('profileId'));
 
-function Clubs() {
-    const { data: clubs, isLoading, error } = useFetch(`${backendUrl}/api/clubs?populate=*`);
-    const profileId = parseInt(localStorage.getItem('profileId'));
-    const [value, setValue] = useState("")
-    const [club, setClub] = useState([])
-    const [search, setSearch] = useState([])
+export default function Clubs() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Example />
+        </QueryClientProvider>
+    );
+}
 
-    useEffect(() => {
-        if (clubs) {
-            setSearch(clubs.data.filter(club => !club.attributes.favouriteProfiles.data.find(item => item.id === profileId)))
-            setClub(clubs.data.filter(club => club.attributes.favouriteProfiles.data.find(item => item.id === profileId)))
-        }
+function Example() {
+    const { handleSubmit, reset } = useForm();
+    const [newClub, setNewClub] = useState();
 
-    }, [clubs, profileId]);
+    const { data: clubs, isLoading, error } = useQuery("clubs", async () => {
+        const data = await fetch(`${backendUrl}/api/clubs?populate=*`).then(r => r.json());
+        return data;
+    });
 
-
-    const addFavourite = (value) => {
-        const selectedClub = clubs.data.filter(club => club.attributes.name === value)
-        const favourites = club.map(item => item.id)
-        favourites.push(selectedClub[0].id)
-        fetch(`${backendUrl}/api/profiles/${profileId}`, {
-            method: 'PUT',
+    const putFavouriteClub = async (data) => {
+        const favourites = clubs.data.filter(club => club.attributes.favouriteProfiles.data.find(item => item.id === profileId)).map(item => item.id)
+        favourites.push(data.id)
+        return await fetch(`${backendUrl}/api/profiles/${profileId}`, {
+            method: "PUT",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({ data: { favouriteClubs: favourites } }),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-                window.location.reload();
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+        }).then(r => r.json());
+    }
+
+    const mutation = useMutation(putFavouriteClub, {
+        onSuccess: () => {
+            console.log("success")
+            queryClient.invalidateQueries("clubs");
+            reset()
+        },
+    })
+
+    const onSubmit = () => {
+        const selectedClub = clubs.data.filter(club => club.attributes.name === newClub)
+        mutation.mutate(selectedClub[0])
     }
 
     if (isLoading) {
@@ -52,28 +60,31 @@ function Clubs() {
             <CircularProgress />
         </Box>
     }
+
     return (
         <>
             <Typography variant="title" component="h1">Clubs</Typography>
             <Box sx={{ color: '#fff', background: 'linear-gradient(0.25turn, #FF9D26, #FE7000);', paddingBottom: '1.5rem' }}>
-                {error && <Alert severity="error">Something went wrong with loading your profile</Alert>}
+                {error && <Alert severity="error">Something went wrong with loading the clubs</Alert>}
                 <Typography variant="h2" component="h2">My favourite clubs</Typography>
                 <Grid container>
-                    {club && club.map(club => <Club key={club.id} club={club} />)}
+                    {clubs && clubs.data.filter(club => club.attributes.favouriteProfiles.data.find(item => item.id === profileId)).map(club => <Club key={club.id} club={club} />)}
                 </Grid>
             </Box>
-            <Typography variant="h2" component="h2">Search for a club!</Typography>
-            <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={search.map(club => club.attributes.name)}
-                sx={{ width: 300, margin: '1rem' }}
-                onChange={(event, value) => setValue(value)}
-                renderInput={(params) => <TextField {...params} label="Search Club" />}
-            />
-            <Button sx={{ margin: '1rem' }} disableElevation variant="outlined" disabled={!value} onClick={() => addFavourite(value)}>Add to favourites</Button>
+            <Typography variant="h2" component="h2">Search for a club</Typography>
+            <Stack as="form" noValidate onSubmit={handleSubmit(onSubmit)}>
+                <Autocomplete
+                    id="club"
+                    disablePortal
+                    required
+                    options={clubs.data.filter(club => !club.attributes.favouriteProfiles.data.find(item => item.id === profileId)).map(club => club.attributes.name)}
+                    sx={{ margin: '1rem' }}
+                    onChange={(event, value) => setNewClub(value)}
+                    renderInput={(params) => <TextField {...params} label="Search Club" />}
+                />
+                <Button type="submit" sx={{ margin: '1rem' }} disabled={!newClub} disableElevation variant="outlined">Add to favourites</Button>
+            </Stack>
         </>
+
     );
 }
-
-export default Clubs;
